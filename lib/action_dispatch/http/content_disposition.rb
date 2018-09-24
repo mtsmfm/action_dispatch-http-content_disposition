143f5b1684eb58b319e55338ef584d6e27f7340e
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2018-09-24 16:02:55 +0900

    $ bundle gem

---
+++ lib/action_dispatch/http/content_disposition.rb
@@ -0,0 +1,9 @@
+require "action_dispatch/http/content_disposition/version"
+
+module ActionDispatch
+  module Http
+    module ContentDisposition
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/action_dispatch/http/content_disposition/version.rb
@@ -0,0 +1,7 @@
+module ActionDispatch
+  module Http
+    module ContentDisposition
+      VERSION = "0.1.0"
+    end
+  end
+end
